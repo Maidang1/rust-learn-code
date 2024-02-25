51f5d71cdf9c6a3aa691057523e9145a9bde81af
maidang1 <497350746@qq.com>
2024-02-25 17:35:54 +0800

    init project

---
+++ npm/darwin-x64/README.md
@@ -0,0 +1,3 @@
+# `farm-start-darwin-x64`
+
+This is the **x86_64-apple-darwin** binary for `farm-start`
---
+++ packages/cli/src/index.js
@@ -0,0 +1,4 @@
+const { sum }  = require('../binding/binding.cjs')
+
+const total = sum(1,2);
+console.log(total)
