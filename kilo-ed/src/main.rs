91a7a29134d64184af656c0419fe0fc1055255cf
maidang1 <497350746@qq.com>
2024-03-26 00:41:54 +0800

    update

--- kilo-ed/src/main.rs
+++ kilo-ed/src/main.rs
@@ -1,15 +1,19 @@
-use errno::errno;
+mod input;
+mod keyboard;
+mod output;
+
 
 use crossterm::{event::Event::*, terminal, Result};
-
-mod keyboard;
-
-mod input;
 use input::*;
+use output::{die, editor_refresh_screen};
 
 fn main() -> Result<()> {
     terminal::enable_raw_mode()?;
     loop {
+        if editor_refresh_screen().is_err() {
+            die("unable to refresh screen");
+        }
+
         if editor_process_keypress() {
             break;
         }
@@ -17,10 +21,4 @@ fn main() -> Result<()> {
     terminal::disable_raw_mode()?;
 
     Ok(())
-}
-
-pub fn die<S: Into<String>>(message: S) {
-    let _ = terminal::disable_raw_mode();
-    eprintln!("{}: {}", message.into(), errno());
-    std::process::exit(1);
 }
